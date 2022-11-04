f7958190160bebcac619f78963ab9ed1d2bd09fa
Artem Pishchulin <art.pish@ya.ru>
2022-11-04 15:06:46 +0300

    9 sprint add PEP8

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -32,9 +32,9 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-   'AUTH_HEADER_TYPES': ('Bearer',),
-} 
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
